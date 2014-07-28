a88eab27beff78399e9ec3924b60e118338e675e
Niklas <niklas@panarchia.(none)>
2014-07-28 03:18:46 +0300

    switch coin_port to mainnet

--- src/tor/rendservice.c
+++ src/tor/rendservice.c
@@ -378,13 +378,13 @@ rend_config_services(const or_options_t *options, int validate_only)
                 rend_service_port_config_t
             )
         );
-        /*
-        coin_port->virtual_port = 35552
-        coin_port->real_port = 35552
-        */
+
+        coin_port->virtual_port = 35552;
+        coin_port->real_port = 35552;
+
         //testnet
-        coin_port->virtual_port = 35553;
-        coin_port->real_port = 35553;
+        //coin_port->virtual_port = 35553;
+        //coin_port->real_port = 35553;
         coin_port->real_addr.family = AF_INET;
         tor_inet_aton(
             "127.0.0.1",
